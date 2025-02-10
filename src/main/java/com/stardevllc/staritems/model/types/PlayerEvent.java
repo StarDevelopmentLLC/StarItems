04ed8d486d03b08353bf9c2933dbede4e1af4a94
Firestar311 <slaterandrew0398@gmail.com>
2025-02-09 21:01:33 -0600

    Added a PlayerInteract event type

--- src/main/java/com/stardevllc/staritems/model/types/PlayerEvent.java
+++ src/main/java/com/stardevllc/staritems/model/types/PlayerEvent.java
@@ -2,9 +2,11 @@ package com.stardevllc.staritems.model.types;
 
 import com.stardevllc.staritems.model.EventType;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 
 public class PlayerEvent<T extends org.bukkit.event.player.PlayerEvent> extends EventType<T> {
     public static final PlayerEvent<PlayerItemConsumeEvent> CONSUME = new PlayerEvent<>();
-    public static final PlayerEvent<PlayerInteractEntityEvent> INTERACT = new PlayerEvent<>();
+    public static final PlayerEvent<PlayerInteractEntityEvent> INTERACT_ENTITY = new PlayerEvent<>();
+    public static final PlayerEvent<PlayerInteractEvent> INTERACT = new PlayerEvent<>();
 }
