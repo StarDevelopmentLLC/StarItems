f717ca8b2fd7a67e552da77afcf9fef8d0f37e9a
Firestar311 <slaterandrew0398@gmail.com>
2025-11-10 04:46:30 -0600

    Move to using libby and updated StarMCLib to 0.11.0

--- standalone/src/main/java/com/stardevllc/staritems/plugin/StandaloneStarItemsPlugin.java
+++ standalone/src/main/java/com/stardevllc/staritems/plugin/StandaloneStarItemsPlugin.java
@@ -4,8 +4,16 @@ import com.stardevllc.staritems.StarItems;
 import com.stardevllc.staritems.cmd.StarItemsCommand;
 import com.stardevllc.starmclib.StarMCLib;
 import com.stardevllc.starmclib.plugin.ExtendedJavaPlugin;
+import net.byteflux.libby.BukkitLibraryManager;
+
+import java.io.File;
 
 public class StandaloneStarItemsPlugin extends ExtendedJavaPlugin {
+    public StandaloneStarItemsPlugin() {
+        BukkitLibraryManager bukkitLibraryManager = new BukkitLibraryManager(this, new File(".", "plugins").toPath(), "libraries");
+        bukkitLibraryManager.configureFromJSON();
+    }
+    
     @Override
     public void onEnable() {
         StarMCLib.init(this);
