9cfbb174cb1c9086f4abd6260551c7c4f64abc82
Firestar311 <slaterandrew0398@gmail.com>
2025-11-14 18:32:47 -0600

    Reverted the libby changes

--- plugin/src/main/java/com/stardevllc/staritems/plugin/StarItemsPlugin.java
+++ plugin/src/main/java/com/stardevllc/staritems/plugin/StarItemsPlugin.java
@@ -3,16 +3,8 @@ package com.stardevllc.staritems.plugin;
 import com.stardevllc.staritems.StarItems;
 import com.stardevllc.staritems.cmd.StarItemsCommand;
 import com.stardevllc.starmclib.plugin.ExtendedJavaPlugin;
-import net.byteflux.libby.BukkitLibraryManager;
-
-import java.io.File;
 
 public class StarItemsPlugin extends ExtendedJavaPlugin {
-    
-    public StarItemsPlugin() {
-        BukkitLibraryManager bukkitLibraryManager = new BukkitLibraryManager(this, new File(".", "plugins").toPath(), "libraries");
-        bukkitLibraryManager.configureFromJSON();
-    }
     
     @Override
     public void onEnable() {
--- standalone/src/main/java/com/stardevllc/staritems/plugin/StandaloneStarItemsPlugin.java
+++ standalone/src/main/java/com/stardevllc/staritems/plugin/StandaloneStarItemsPlugin.java
@@ -4,18 +4,20 @@ import com.stardevllc.staritems.StarItems;
 import com.stardevllc.staritems.cmd.StarItemsCommand;
 import com.stardevllc.starmclib.StarMCLib;
 import com.stardevllc.starmclib.plugin.ExtendedJavaPlugin;
-import net.byteflux.libby.BukkitLibraryManager;
-
-import java.io.File;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 public class StandaloneStarItemsPlugin extends ExtendedJavaPlugin {
-    public StandaloneStarItemsPlugin() {
-        BukkitLibraryManager bukkitLibraryManager = new BukkitLibraryManager(this, new File(".", "plugins").toPath(), "libraries");
-        bukkitLibraryManager.configureFromJSON();
-    }
-    
     @Override
     public void onEnable() {
+        Plugin starmclibPlugin = Bukkit.getPluginManager().getPlugin("StarMCLib");
+        if (starmclibPlugin != null) {
+            getLogger().severe("StarMCLib plugin detected with the StarItems-Standalone plugin");
+            getLogger().severe("Please either replace StarItems-Standalone with StarItems Plugin or remove StarMCLib plugin");
+            getLogger().severe("Please see the wiki page for more information");
+            getLogger().severe("https://github.com/StarDevelopmentLLC/StarItems/wiki/Available-Binaries");
+        }
+        
         StarMCLib.init(this);
         super.onEnable();
         StarItems.init(this);
